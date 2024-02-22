613b221d146e670cc064b8292795e64a749c8faf
Goodbird <goodbirdmail@yandex.ru>
2024-02-22 13:07:00 +0300

    Changed database to using singleton pattern

--- src/main/java/com/vvi/restaurantserver/database/DatabaseManager.java
+++ src/main/java/com/vvi/restaurantserver/database/DatabaseManager.java
@@ -1,21 +1,43 @@
 package com.vvi.restaurantserver.database;
 
 import com.vvi.restaurantserver.config.Config;
+import com.vvi.restaurantserver.database.tables.DishManager;
 import com.vvi.restaurantserver.database.tables.UserManager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseManager {
     private Connection connection;
     public UserManager userManager;
+    public DishManager dishManager;
+
+    private static DatabaseManager instance;
+
+    public static DatabaseManager getInstance(){
+        if(instance==null){
+            instance = new DatabaseManager();
+        }
+        return instance;
+    }
+
+    private DatabaseManager(){
+
+    }
 
     public boolean init() {
         final String url = "jdbc:mysql://" + Config.getDatabaseHost() + ":" + Config.getDatabasePort() + "/" + Config.getDatabaseName();
         try {
-            connection = DriverManager.getConnection(url, Config.getDatabaseLogin(), Config.getDatabasePassword());
+            Properties properties = new Properties();
+            properties.setProperty("user", Config.getDatabaseLogin());
+            properties.setProperty("password", Config.getDatabasePassword());
+            properties.setProperty("useUnicode", "true");
+            properties.setProperty("characterEncoding", "UTF-8");
+            connection = DriverManager.getConnection(url, properties);
             userManager = new UserManager(connection);
+            dishManager = new DishManager(connection);
             return true;
         } catch (SQLException e) {
             return false;
