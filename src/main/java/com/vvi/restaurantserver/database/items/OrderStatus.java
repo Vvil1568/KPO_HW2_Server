5768560522bf338cd1107e38c829ff2316ec0669
Goodbird <goodbirdmail@yandex.ru>
2024-02-24 12:04:14 +0300

    Changed order status

--- src/main/java/com/vvi/restaurantserver/database/items/OrderStatus.java
+++ src/main/java/com/vvi/restaurantserver/database/items/OrderStatus.java
@@ -1,7 +1,8 @@
 package com.vvi.restaurantserver.database.items;
 
 public enum OrderStatus {
-    STARTED("составляется"),
+    STARTED("формируется"),
+    ACCEPTED("принят"),
     PLACED("готовится"),
     COOKED("готов и ожидает оплаты"),
     PAID("оплачен");
